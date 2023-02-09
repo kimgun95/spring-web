9e37485be172ceceb5d99459fe41569cf3b47ad2
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-02-09 17:47:32 +0900

    #12 - Data Rest 설정
    Article, ArticleComment 의 RESTful api가 자동으로 생성되게 Data Rest 설정

--- src/main/java/com/example/springweb/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/springweb/repository/ArticleCommentRepository.java
@@ -2,7 +2,9 @@ package com.example.springweb.repository;
 
 import com.example.springweb.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 }
--- src/main/java/com/example/springweb/repository/ArticleRepository.java
+++ src/main/java/com/example/springweb/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.example.springweb.repository;
 
 import com.example.springweb.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
