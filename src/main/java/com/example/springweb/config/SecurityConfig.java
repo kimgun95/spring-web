4908387efd017bbd77a02e1393025693cc6db1c7
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-02-10 17:06:09 +0900

    #13 - 시큐리티 초기 설정
    모든 요청에 인증이 열리도록 설정
    최신 스프링 부트 2.7의 시큐리티 설정 방법을 이용
    https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

---
+++ src/main/java/com/example/springweb/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.springweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin();
+
+        return httpSecurity.build();
+    }
+}
