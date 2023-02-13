df336619bb3074dc5b0aa8750b34b593d4eb4e0a
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-02-13 22:35:26 +0900

    UserAccount 도메인의 id 삭제 및 userId를 유니크 키로 변경
    해당 변경으로 인해 연관된 이슈들 모두 수정

--- src/test/java/com/example/springweb/service/ArticleCommentServiceTest.java
+++ src/test/java/com/example/springweb/service/ArticleCommentServiceTest.java
@@ -145,7 +145,6 @@ public class ArticleCommentServiceTest {
 
     private UserAccountDto createUserAccountDto() {
         return UserAccountDto.of(
-                1L,
                 "uno",
                 "password",
                 "uno@mail.com",
--- src/test/java/com/example/springweb/service/ArticleServiceTest.java
+++ src/test/java/com/example/springweb/service/ArticleServiceTest.java
@@ -243,7 +243,6 @@ public class ArticleServiceTest {
 
     private UserAccountDto createUserAccountDto() {
         return UserAccountDto.of(
-                1L,
                 "uno",
                 "password",
                 "uno@mail.com",
