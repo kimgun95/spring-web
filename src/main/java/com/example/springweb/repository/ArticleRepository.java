f24a32958fa44eca48e2518adfcfad4c4d9af246
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-02-13 19:38:19 +0900

    #21 - hashtag 검색 페이지를 위한 기능 구현
    게시글에 존재하는 해시태그를 모두 정리하여 보여주는 기능을 넣기 위해 querydsl로 구현
    관련하여 service, controller, 테스트 코드 작성

--- src/main/java/com/example/springweb/repository/ArticleRepository.java
+++ src/main/java/com/example/springweb/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.example.springweb.repository;
 
 import com.example.springweb.domain.Article;
 import com.example.springweb.domain.QArticle;
+import com.example.springweb.repository.querydsl.ArticleRepositoryCustom;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.domain.Page;
@@ -15,6 +16,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
+        ArticleRepositoryCustom,
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
 
---
+++ src/main/java/com/example/springweb/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,25 @@
+package com.example.springweb.repository.querydsl;
+
+import com.example.springweb.domain.Article;
+import com.example.springweb.domain.QArticle;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom{
+
+    public ArticleRepositoryCustomImpl() {
+        super(Article.class);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtags() {
+        QArticle article = QArticle.article;
+
+        return from(article)
+                .distinct()
+                .select(article.hashtag)
+                .where(article.hashtag.isNotNull())
+                .fetch();
+    }
+}
