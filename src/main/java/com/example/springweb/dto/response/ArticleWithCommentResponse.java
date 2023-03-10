718347ee4df26d0aa559ddb1e806f3ec22794461
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-03-10 23:25:41 +0900

    #36 - 대댓글 비즈니스 로직 구현
    `ArticleComment` 도메인 수정으로 인해
    - dto,request,response간 변환 로직
    - `ArticleCommentService`에서 대댓글 저장,삭제 방법
    - 댓글과 대댓글을 db 데이터로부터 변환시 서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- src/main/java/com/example/springweb/dto/response/ArticleWithCommentResponse.java
+++ src/main/java/com/example/springweb/dto/response/ArticleWithCommentResponse.java
@@ -1,11 +1,12 @@
 package com.example.springweb.dto.response;
 
+import com.example.springweb.dto.ArticleCommentDto;
 import com.example.springweb.dto.ArticleWithCommentDto;
 import com.example.springweb.dto.HashtagDto;
 
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentResponse(
@@ -42,10 +43,31 @@ public record ArticleWithCommentResponse(
                 dto.userAccountDto().email(),
                 nickname,
                 dto.userAccountDto().userId(),
-                dto.articleCommentDtos().stream()
-                        .map(ArticleCommentResponse::from)
-                        .collect(Collectors.toCollection(LinkedHashSet::new))
+                organizeChildComments(dto.articleCommentDtos())
         );
+    }
+
+    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+        Map<Long, ArticleCommentResponse> map = dtos.stream()
+                .map(ArticleCommentResponse::from)
+                .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity()));
+
+        map.values().stream()
+                .filter(ArticleCommentResponse::hasParentComment)
+                .forEach(comment -> {
+                    ArticleCommentResponse parentComment = map.get(comment.parentCommentId());
+                    parentComment.childComments().add(comment);
+                });
+
+        return map.values().stream()
+                .filter(comment -> !comment.hasParentComment())
+                .collect(Collectors.toCollection(() ->
+                        new TreeSet<>(Comparator
+                                .comparing(ArticleCommentResponse::createdAt)
+                                .reversed()
+                                .thenComparingLong(ArticleCommentResponse::id)
+                        )
+                ));
     }
 
 }
